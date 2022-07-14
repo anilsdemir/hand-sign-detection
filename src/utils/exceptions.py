40c67ff66e985101e195ceaaf77299ce15be8e82
Anil Demir <anilsdemir@gmail.com>
2022-07-15 00:45:15 +0300

    CustomException class and some custom exceptions are created

---
+++ src/utils/exceptions.py
@@ -0,0 +1,8 @@
+class CustomException(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(message)
+
+
+class VideoCanNotBeShown(CustomException):
+    pass
